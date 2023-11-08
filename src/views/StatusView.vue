ae871aaadc246ce03e564b16f40deb0f5b8812c0
Nagy Attila (aha) <nagy.attis.81@gmail.com>
2023-11-08 21:05:44 +0100

    feat: axios

--- src/views/StatusView.vue
+++ src/views/StatusView.vue
@@ -3,3 +3,17 @@
     <h1>This is an about page</h1>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import axios from "axios";
+import { API_URL } from "@/constants/api";
+
+axios.get(API_URL).then((response) => {
+  console.log(response.data);
+});
+
+export default defineComponent({
+  name: "StatusView",
+});
+</script>
