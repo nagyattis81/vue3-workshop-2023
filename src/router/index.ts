1c78255f212e1e09aa1e870d9c280d9886e36434
Nagy Attila (aha) <nagy.attis.81@gmail.com>
2023-11-08 21:10:28 +0100

    refactor: use constants for router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,21 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 import LoginView from "../views/LoginView.vue";
+import {
+  LOGIN_PATH,
+  LOGIN_ROUTE,
+  STATUS_PATH,
+  STATUS_ROUTE,
+} from "@/constants/routes";
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "login",
+    path: LOGIN_PATH,
+    name: LOGIN_ROUTE,
     component: LoginView,
   },
   {
-    path: "/status",
-    name: "status",
+    path: STATUS_PATH,
+    name: STATUS_ROUTE,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
