580cca493fcc5e5de0f0b99c10c25c078ce60782
Nagy Attila (aha) <nagy.attis.81@gmail.com>
2023-11-09 08:47:05 +0100

    feat: h1 title

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,7 @@
 <template>
+  <h1 class="d-flex justify-content-center container p-4 mt-4">
+    Vue3 workshop @ 2023
+  </h1>
   <div class="container shadow p-4 mt-4 bg-light">
     <router-view />
   </div>
